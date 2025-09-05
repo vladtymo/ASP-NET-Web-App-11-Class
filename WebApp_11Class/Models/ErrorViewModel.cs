2fbd16dd6642be4e11298b96ea11151062e3cd10
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-09-05 09:59:20 +0300

    Add project files.

---
+++ WebApp_11Class/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_11Class.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
