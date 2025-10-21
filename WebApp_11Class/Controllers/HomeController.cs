2d0b2a9ddf4beb52ac66e93f054cd30561b66177
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-10-21 15:13:11 +0300

    Up

--- WebApp_11Class/Controllers/HomeController.cs
+++ WebApp_11Class/Controllers/HomeController.cs
@@ -8,10 +8,19 @@ namespace WebApp_11Class.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        static List<Product> products = new()
+        {
+            new Product { Id = 1, Title = "Laptop", Category = "Electronics", Price = 1200, Quantity = 10 },
+            new Product { Id = 2, Title = "Desk Chair", Category = "Furniture", Price = 150, Quantity = 25 },
+            new Product { Id = 3, Title = "Notebook", Category = "Stationery", Price = 5, Quantity = 100 },
+            new Product { Id = 4, Title = "Smartphone", Category = "Electronics", Price = 800, Quantity = 15 },
+            new Product { Id = 5, Title = "Coffee Mug", Category = "Kitchenware", Price = 12, Quantity = 50 }
+        };
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }   
 
         public IActionResult Index()
         {
@@ -25,7 +34,20 @@ namespace WebApp_11Class.Controllers
 
         public IActionResult AdminPanel()
         {
-            return View();
+            return View(products);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            // delete logic
+            var item = products.Find(x => x.Id == id);
+
+            if (item == null)
+                return NotFound(); // 404
+
+            products.Remove(item);
+
+            return RedirectToAction("AdminPanel");
         }
 
         public IActionResult Products()
---
+++ WebApp_11Class/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp_11Class.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
