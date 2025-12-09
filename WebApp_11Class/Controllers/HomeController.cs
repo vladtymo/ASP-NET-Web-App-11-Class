1b78953786646d947d63247eedbd6d33a31a775d
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-12-09 16:30:55 +0200

    Update

--- WebApp_11Class/Controllers/HomeController.cs
+++ WebApp_11Class/Controllers/HomeController.cs
@@ -10,10 +10,10 @@ namespace WebApp_11Class.Controllers
 
         static List<Product> products = new()
         {
-            new Product { Id = 1, Title = "Laptop", Category = "Electronics", Price = 1200, Quantity = 10 },
-            new Product { Id = 2, Title = "Desk Chair", Category = "Furniture", Price = 150, Quantity = 25 },
+            new Product { Id = 1, Title = "Laptop", Category = "Electronics", Price = 1200, Quantity = 10, ImageUrl = "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA5L3Jhd3BpeGVsX29mZmljZV8zMV9waG90b19vZl9hX2xhcHRvcF9tb2NrdXBfY2xvc2UtdXBfbWluaW1hbF9pc182M2Q2NzViOS00YjlhLTQ3OWEtOGMyMS1hYWQwMjViNWYzZDIucG5n.png" },
+            new Product { Id = 2, Title = "Desk Chair", Category = "Furniture", Price = 150, Quantity = 25, ImageUrl = "https://m.media-amazon.com/images/I/61VqPRU2-UL._AC_UF894,1000_QL80_.jpg" },
             new Product { Id = 3, Title = "Notebook", Category = "Stationery", Price = 5, Quantity = 100 },
-            new Product { Id = 4, Title = "Smartphone", Category = "Electronics", Price = 800, Quantity = 15 },
+            new Product { Id = 4, Title = "Smartphone", Category = "Electronics", Price = 800, Quantity = 15, ImageUrl="https://cdn.thewirecutter.com/wp-content/media/2025/09/BG-IPHONE-2048px_IPHONE-17-PRO-MAX_BACK.jpg?auto=webp&quality=75&width=1024" },
             new Product { Id = 5, Title = "Coffee Mug", Category = "Kitchenware", Price = 12, Quantity = 50 }
         };
 
@@ -37,9 +37,10 @@ namespace WebApp_11Class.Controllers
             return View();
         }
 
-        public IActionResult Details()
+        public IActionResult Details(int id)
         {
-            return View();
+            var item = products.FirstOrDefault(x => x.Id == id);
+            return View(item);
         }
 
         public IActionResult Edit(int id)
--- WebApp_11Class/Models/Product.cs
+++ WebApp_11Class/Models/Product.cs
@@ -7,5 +7,6 @@
         public string Category { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
